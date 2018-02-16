52d94d106a9c6ddd18033c33a76c9d8852d3f380
이재범 <btv9200@gmail.com>
2018-02-17 00:41:51 +0900

    save html data use Jsoup, RxJava

--- app/src/main/java/lee/jaebaom/qrcodereader/MainActivity.kt
+++ app/src/main/java/lee/jaebaom/qrcodereader/MainActivity.kt
@@ -2,24 +2,30 @@ package lee.jaebaom.qrcodereader
 
 import android.content.Intent
 import android.os.Bundle
+import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
-import android.widget.Toast
 import com.google.zxing.integration.android.IntentIntegrator
 import com.google.zxing.integration.android.IntentResult
+import io.reactivex.Observable
+import io.reactivex.Scheduler
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
 
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.content_main.*
+import org.jsoup.Jsoup
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
+    private val histories = ArrayList<History>()
+    private val adapter = MainRecyclerAdapter(histories)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
-        val histories = ArrayList<History>()
-        val adapter = MainRecyclerAdapter(histories)
         recycler.adapter = adapter
 
         fab.setOnClickListener { view ->
@@ -48,11 +54,28 @@ class MainActivity : AppCompatActivity() {
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         val intentResult: IntentResult? = IntentIntegrator.parseActivityResult(requestCode, resultCode, data)
-        if (intentResult?.contents == null){
-            Toast.makeText(this, "Cancelled", Toast.LENGTH_LONG).show();
-        }else{
-            Toast.makeText(this, "Success ${intentResult.contents}", Toast.LENGTH_LONG).show()
-        }
+        extractTitle(intentResult?.contents!!)
+                .subscribeOn(Schedulers.computation())
+                .observeOn(Schedulers.newThread())
+                .map{
+                    onNext ->
+                        histories.add(History(onNext, intentResult.contents))
+                }
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe {
+
+                    adapter.notifyDataSetChanged()
+                }
+
+
         super.onActivityResult(requestCode, resultCode, data)
+    }
+
+    private fun extractTitle(URL: String) : Observable<String>{
+        return Observable.create<String> {subscriber ->
+            val doc = Jsoup.connect(URL).get()
+            subscriber.onNext(doc.title())
+        }
+
     }
 }
